0bd004e36db4ab4722591dfe99e4465440768a42
robinyeon <robin.yeon@gmail.com>
2022-02-28 21:35:34 +0900

    Seperated categories with Recoil selectors

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,16 +1,6 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
-import { ITodo, toDoState } from "./atoms";
-
-// **제 위치에 있는 요소를 새로운 요소(값)로 대체하는 방법**
-// (1) 타겟의 인덱스를 파악한다(어디있는지)
-// (2) 타겟이 오기 직전의 앞부분(front)과 뒷부분(back)으로 나눈다.
-// (3) final = [...front, "새로운요소", ...back]
-// const food = ["pizza", "mango", "kimchi", "kimbab"];
-// const target = 1;
-// food.slice(0, target); // ["pizza"]
-// food.slice(target + 1); //["kimchi", "kimbab"]
-// [...food.slice(0, target), "감", ...food.slice(target + 1)];
+import { Categories, ITodo, toDoState } from "./atoms";
 
 const ToDo = ({ text, category, id }: ITodo) => {
   const setToDos = useSetRecoilState(toDoState);
@@ -31,18 +21,18 @@ const ToDo = ({ text, category, id }: ITodo) => {
   return (
     <li>
       <span>{text}</span>
-      {category !== "DOING" && (
-        <button name="DOING" onClick={onClick}>
+      {category !== Categories.DOING && (
+        <button name={Categories.DOING} onClick={onClick}>
           Doing
         </button>
       )}
-      {category !== "TO_DO" && (
-        <button name="TO_DO" onClick={onClick}>
+      {category !== Categories.TO_DO && (
+        <button name={Categories.TO_DO} onClick={onClick}>
           To Do
         </button>
       )}
-      {category !== "DONE" && (
-        <button name="DONE" onClick={onClick}>
+      {category !== Categories.DONE && (
+        <button name={Categories.DONE} onClick={onClick}>
           Done
         </button>
       )}
